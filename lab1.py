36574ec14d2cb3c51d39c069968b26c1d875af9a
patthananp <87537221+patthananp@users.noreply.github.com>
2021-08-31 21:30:33 +0700

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,47 @@
+import turtle
+
+s = turtle.getscreen()
+turtle.color("black","red")
+turtle.begin_fill()
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(60)
+turtle.end_fill()
+
+turtle.right(90)
+
+turtle.forward(300)
+turtle.right(90)
+
+turtle.color("black","navy")
+turtle.begin_fill()
+turtle.forward(30)
+
+
+turtle.backward(80)
+
+turtle.left(90)
+turtle.backward(300)
+
+turtle.right(90)
+turtle.forward(50)
+turtle.backward(80)
+# turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(30)
+
+turtle.backward(60)
+turtle.left(90)
+turtle.backward(300)
+turtle.right(90)
+turtle.forward(30)
+
+# turtle.end_fill()
+turtle.done()
