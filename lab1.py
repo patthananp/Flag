41abf67cb1c654c31cb158cd8284932c7762b226
patthananp <87537221+patthananp@users.noreply.github.com>
2021-09-01 21:09:52 +0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 import turtle
 
 s = turtle.getscreen()
+
 turtle.color("black","red")
 turtle.begin_fill()
 turtle.forward(300)
@@ -9,39 +10,42 @@ turtle.forward(30)
 turtle.right(90)
 turtle.forward(300)
 turtle.right(90)
-turtle.forward(60)
+turtle.forward(30)
 turtle.end_fill()
 
+turtle.forward(30)
 turtle.right(90)
-
 turtle.forward(300)
 turtle.right(90)
+turtle.forward(30)
+turtle.backward(30)
 
 turtle.color("black","navy")
 turtle.begin_fill()
-turtle.forward(30)
-
-
-turtle.backward(80)
-
+turtle.backward(50)
 turtle.left(90)
 turtle.backward(300)
-
-turtle.right(90)
-turtle.forward(50)
-turtle.backward(80)
-# turtle.end_fill()
-
 turtle.left(90)
+turtle.backward(50)
+turtle.end_fill()
+
+turtle.forward(50)
+
+
+turtle.forward(30)
+turtle.right(90)
 turtle.forward(300)
 turtle.right(90)
 turtle.forward(30)
+turtle.backward(30)
 
-turtle.backward(60)
-turtle.left(90)
-turtle.backward(300)
+turtle.color("black","red")
+turtle.begin_fill()
+turtle.backward(30)
 turtle.right(90)
+turtle.forward(300)
+turtle.left(90)
 turtle.forward(30)
+turtle.end_fill()
 
-# turtle.end_fill()
 turtle.done()
